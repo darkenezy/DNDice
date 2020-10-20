5c9b591b7b8f225990304e20d8419b90340bf3c7
Daniel Korotkov <dkorotkov@bestplace.pro>
2020-10-21 00:41:19 +0300

    global update (front + api reworked)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.7-alpine
 COPY app /app
 WORKDIR /app
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-CMD ["gunicorn", "-w 4", "server:app", "--bind", "0.0.0.0"]
+CMD ["gunicorn", "-w 1", "server:app", "--bind", "0.0.0.0", "--reload"]
--- app/server.py
+++ app/server.py
@@ -1,42 +1,123 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, redirect, jsonify, render_template
+from collections import OrderedDict
 from random import randint
+import pydash as _
 import re
 
 
 class Dice:
     def __init__(self, d):
-        self.d = d
+        self._d = d or ""
+        self._value = None
+        self._faces = None
 
-    @property
     def is_valid(self):
-        return re.match(r"[Dd]([1-9][0-9]*)", self.d)
+        return re.match(r"[Dd]([1-9][0-9]*)", self._d)
 
     @property
     def faces(self):
-        match = self.is_valid
-        if not match:
-            return 0
-        return int(match.group(1))
+        if self._faces is None:
+            match = self.is_valid()
+            self._faces = int(match.group(1)) if match else 0
+        return self._faces
 
-    @property
-    def value(self):
-        return randint(1, self.faces)
+    def roll(self):
+        self._value = randint(1, self.faces)
+        return self._value
+
+    def to_representation(self):
+        return {"d": self._d, "value": self._value}
 
 
-app = Flask(__name__)
+# Мне лень сюда постгрес приколачивать
+db = {
+    "9999": {  # Example entry
+        "dice": {
+            "123": Dice("D8"),
+            "323": Dice("D10")
+        },
+    }
+}
 
 
-@app.route('/dice')
-def throw_dice():
-    response = []
-    for d, count in request.args.items():
-        dice = Dice(d)
-        count = int(count)
-        if not dice.is_valid:
-            return jsonify({'error': f'{d} is not ad valid dice'}), 400
-        response.append([dice.value for i in range(count)])
+# Flask app
+app = Flask(__name__, static_url_path="/static")
+
+
+# Util
+def get_auth_code(request, allow_create=False):
+    auth_code = request.cookies.get("auth_code")
+    if allow_create and not auth_code:
+        auth_code = str(randint(10000, 99999))
+    return auth_code
+
+
+@app.route("/api/v1/auth")
+def authorize():
+    auth_code = get_auth_code(request, allow_create=True)
+    response = redirect("/game")
+    response.set_cookie("auth_code", auth_code)
+    return response
+
+
+@app.route("/api/v1/get_dice")
+def get_dice():
+    auth_code = get_auth_code(request)
+    if not auth_code:
+        return redirect("/api/v1/auth", status=401)
+
+    db[auth_code] = db.get(auth_code, OrderedDict())
+    response = _.map_values(db[auth_code], Dice.to_representation)
     return jsonify(response), 200
 
 
-if __name__ == '__main__':
+@app.route("/api/v1/add_dice", methods=["POST"])
+def add_dice():
+    auth_code = get_auth_code(request)
+    if not auth_code:
+        return redirect("/api/v1/auth")
+
+    dice_id, dice_type = _.at(request.json, "dice_id", "dice_type")
+
+    dice = Dice(dice_type)
+    if not dice.is_valid():
+        return Response("Invalid dice description", 400)
+
+    db[auth_code] = db.get(auth_code, OrderedDict())
+    db[auth_code][dice_id] = dice
+    return Response("OK", 201)
+
+
+@app.route("/api/v1/remove_dice", methods=["POST"])
+def remove_dice():
+    auth_code = get_auth_code(request)
+    if not auth_code:
+        return redirect("/api/v1/auth", status=401)
+
+    dice_id = request.json.get("dice_id")
+    if _.get(db, f"{auth_code}.{dice_id}"):
+        del db[auth_code][dice_id]
+        return Response("OK", 204)
+    return Response("Invalid dice_id", 400)
+
+
+@app.route("/api/v1/roll", methods=["POST"])
+def roll_dice():
+    auth_code = get_auth_code(request)
+    if not auth_code:
+        return redirect("/api/v1/auth", status=401)
+
+    db[auth_code] = db.get(auth_code, OrderedDict())
+    for dice in db[auth_code].values():
+        dice.roll()
+
+    return Response("OK", 200)
+
+
+@app.route("/game")
+def game():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
